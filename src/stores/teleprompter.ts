63df2cd17992f030c76171e780f7cf8c67f79d25
Justin Davies <therealjustindude@gmail.com>
2024-12-19 21:57:49 -0500

    added stores

---
+++ src/stores/teleprompter.ts
@@ -0,0 +1,43 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useTeleprompterStore = defineStore('teleprompter', () => {
+  const isPlaying = ref(false)
+  const teleprompterSpeedSlider = ref(DEFAULT_SPEED)
+  const fontSizeSlider = ref(FONT_SIZE)
+
+  function startPlaying() {
+    isPlaying.value = true
+  }
+
+  function stopPlaying() {
+    isPlaying.value = false
+  }
+
+  function updateSpeedSlider(value: string) {
+    teleprompterSpeedSlider.value = value
+  }
+
+  function updateFontSlider(value: string) {
+    fontSizeSlider.value = value
+  }
+
+  function resetTeleprompterSliders() {
+    teleprompterSpeedSlider.value = DEFAULT_SPEED
+    fontSizeSlider.value = FONT_SIZE
+  }
+
+  return {
+    isPlaying,
+    startPlaying,
+    stopPlaying,
+    teleprompterSpeedSlider,
+    fontSizeSlider,
+    updateSpeedSlider,
+    updateFontSlider,
+    resetTeleprompterSliders,
+  }
+})
+
+const DEFAULT_SPEED = '20'
+const FONT_SIZE = '16'
